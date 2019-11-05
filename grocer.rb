7c721ce84052285b2fbb8ca43ace65c5a081024d
Tissiana Alves <altissiana@gmail.com>
2019-11-05 22:25:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,27 +31,59 @@ def apply_coupons(cart, coupons)
   #loop through cart with original lenght of cart array 
   cart_length = cart.length
   for i in 0..cart_length
+  
+    # get the cart item at the 'i' index
     item = cart[i]
+    
+    # loop through each coupon
     coupons.each do |coupon|
+      
+      # if the cart item is the same as the coupon item
       if item[:item] == coupon[:item]
+        
+        # boolean to see if we've already added an entry for this coupon
         discounted_item_found = false
+        
+        # loop through the cart array to check for the coupon item
         cart.each do |inner_item|
+          
+          # if we've found the coupon item
           if inner_item[:item] == "#{item[:item]} W/COUPON"
+            
+            # update the count of the coupon item to reflect the newly applied coupon
             inner_item[:count] += coupon[:num]
+            
+            # and notify the loop that we've found the coupon item
             discounted_item_found = true
           end
+          
+        # decrease the count of the original item by the number in the coupon
         item[:count] -= coupon[:num]
         end
+        
+        # if we did not find a coupon item
         if !discounted_item_found
+          
+          # create a copy of the original item
           discounted_item = item.clone()
+          
+          # update the name so it's 'original item W/COUPON'
           discounted_item[:item] = "#{discounted_item[:item]} W/COUPON"
+          
+          # set the price to the coupon cost divided by the num covered by the coupon
           discounted_item[:price] = coupon[:cost] / coupon[:num]
+          
+          # initialize the coupon item's count to the num of the coupon
           discounted_item[:count] = coupon[:num]
+          
+          # add the coupon item to the cart
           cart.push(discounted_item)
         end
       end
     end
   end
+  
+  # return the cart (Holy Jebeebus)
   cart
 end
 
